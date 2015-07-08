4a6cb53cf9fe033f2efa497b5b2ee44d667fe15d
Ankit Jaiswal <ankitjaiswal07@users.noreply.github.com>
2015-07-08 19:34:22 +0530

    Fixed issue with BooleanField default value.

--- offline/models.py
+++ offline/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext as _
 
 class Config(models.Model):
     key = models.CharField(_('Key'), max_length=100, unique=True)
-    value = models.BooleanField(_('Value'))
+    value = models.BooleanField(_('Value'), default=False)
     message = models.TextField(_('Message'))
 
     def __unicode__(self):
